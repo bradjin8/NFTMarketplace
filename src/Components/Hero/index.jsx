46fbf66aecce1fcf3de91f551df42e2a7c0dceb9
Monarch <monarch@Monarchs-iMac.local>
2022-03-04 00:48:38 +0800

    first commit

---
+++ src/Components/Hero/index.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+import classes from "./Hero.module.css";
+import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
+import contractContainer from "./contract_container.png";
+import contractButton from "./contractButton.png";
+import * as copy from "copy-to-clipboard";
+import Featured from "../Home/Featured";
+const fadeFromLeft = {
+  initial: { x: "-100vh", opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
+const zoomAndFade = {
+  initial: { scale: 0.1, opacity: 0 },
+  visible: {
+    scale: 1,
+    opacity: 1,
+  },
+};
+
+const Hero = () => {
+  let cryptoniteLogo =
+    "https://crpyotonite-game-site.s3.eu-west-2.amazonaws.com/images/logo-large.svg";
+  /* let waterAvatar = "https://crpyotonite-game-site.s3.eu-west-2.amazonaws.com/images/home/alien1.png";
+let fireAvatar = "https://crpyotonite-game-site.s3.eu-west-2.amazonaws.com/images/uncommon_fire.svg";
+let acidAvatar = "https://crpyotonite-game-site.s3.eu-west-2.amazonaws.com/images/uncommon_acid.svg"; */
+
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+  const isMobile = useMediaQuery({ query: "(max-width: 425px)" });
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className={classes.container}>
+      <div className={classes.logoContainer}>
+        <motion.img
+          src={cryptoniteLogo}
+          alt="Cryptonite Logo"
+          variants={fadeFromLeft}
+          initial="initial"
+          animate="visible"
+          transition={{
+            delay: 0.4,
+            duration: 2,
+            type: "spring",
+            stiffness: 100,
+          }}
+        />
+      </div>
+      <div className={classes.scaleincenter} style={{ marginTop: "60px" }}>
+        <div className={classes.contractContainer}>
+          <a>
+            Contract: 0x145812dd822Cca602161e9E1ea96963be290b549
+            <span className={classes.tooltip}>
+              <img
+                src={contractButton}
+                alt=""
+                onClick={() =>
+                  copy("0x145812dd822Cca602161e9E1ea96963be290b549")
+                }
+              />
+              <span className={classes.tooltiptext}>Copied</span>
+            </span>
+          </a>
+          <img src={contractContainer} />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "25px",
+          }}
+        >
+          <iframe
+            style={{ border: "none", borderRadius: "25px" }}
+            src="https://poocoin.app/embed-swap?outputCurrency=0x145812dd822Cca602161e9E1ea96963be290b549"
+            width="420"
+            height="630px"
+          ></iframe>
+        </div>
+      </div>
+      <Featured />
+
+      {/* <div
+        className={classes.avatarContainer}
+        style={{
+          transform: `translateY(${-offsetY * 0.3}px)`,
+        }}
+      >
+        <motion.img
+          drag={isMobile ? false : true}
+          dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+          variants={zoomAndFade}
+          initial="initial"
+          animate="visible"
+          transition={{
+            delay: 0.8,
+            duration: 4,
+            type: "spring",
+            stiffness: 100,
+          }}
+          src={acidAvatar}
+          alt="Acid Avatar"
+        />
+        <motion.img
+          drag={isMobile ? false : true}
+          dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+          variants={zoomAndFade}
+          initial="initial"
+          animate="visible"
+          transition={{ duration: 4, type: "spring", stiffness: 100 }}
+          src={waterAvatar}
+          alt="Water Avatar"
+        />
+        <motion.img
+          drag={isMobile ? false : true}
+          dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+          variants={zoomAndFade}
+          initial="initial"
+          animate="visible"
+          transition={{
+            delay: 1.2,
+            duration: 4,
+            type: "spring",
+            stiffness: 100,
+          }}
+          src={fireAvatar}
+          alt="Fire Avatar"
+        />
+      </div> */}
+    </div>
+  );
+};
+
+export default Hero;
